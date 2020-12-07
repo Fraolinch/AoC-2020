dec03b5af04a75b95075c55bda11aa3f985da2a0
Fraolinch <fraolinch@gmail.com>
2020-12-07 10:12:33 +0100

    remove superfluous operations for day4 part 1

--- day4.py
+++ day4.py
@@ -3,11 +3,6 @@ import re
 with open("input4", 'r') as file:
     passports = file.read().split("\n\n")
 
-rgx = re.compile(r"(\w+):")
-
-keys = ["byr:", "iyr:", "eyr:", "hgt:",
-        "hcl:", "ecl:", "pid:", "cid:"]
-
 valid = 0
 cid = 0
 
@@ -15,10 +10,9 @@ for passport in passports:
     passport = passport.replace(" ", "\n")
     passport = passport.splitlines()
     for pair in passport:
-        key = rgx.match(pair)
-        for k in keys:
-            if key[0] == "cid:":
-                cid = 1
+        if pair[:3] == "cid":
+            cid = 1
+            break
     if len(passport) == 8 or (len(passport) == 7 and cid == 0):
             valid = valid + 1
     cid = 0
